d4b7ba49571deb7e5b73b345ae68583e0f241225
drmonky <nnthai94@gmail.com>
2020-05-25 12:58:08 -0400

    adding files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,118 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SidebarComponent from './sidebar/sidebar';
+import EditorComponent from './editor/editor';
+//import SidebarItemComponent from './sidebar_item/sidebaritems'
+const firebase = require('firebase');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes: null
+    };
+  }
+
+  render() {
+    return (
+      <div className='app-container'>
+        <SidebarComponent
+          selectedNoteIndex={this.state.selectedNoteIndex}
+          notes={this.state.notes}
+          deleteNote={this.deleteNote}
+          selectNote={this.selectNote}
+          newNote={this.newNote}
+        ></SidebarComponent>
+        {this.state.selectedNote ? (
+          <EditorComponent
+            selectedNote={this.state.selectedNote}
+            selectedNoteIndex={this.state.selectedNoteIndex}
+            notes={this.state.notes}
+            noteUpdate={this.noteUpdate}
+          ></EditorComponent>
+        ) : null}
+      </div>
+    );
+  }
+
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection('notes')
+      .onSnapshot((serverupdates) => {
+        const notes = serverupdates.docs.map((_doc) => {
+          const data = _doc.data();
+          data['id'] = _doc.id;
+          return data;
+        });
+        console.log(notes);
+        this.setState({ notes: notes });
+      });
+  };
+  selectNote = (note, index) => {
+    this.setState({ selectedNoteIndex: index, selectedNote: note });
+  };
+  noteUpdate = (id, noteObject) => {
+    firebase
+      .firestore()
+      .collection('notes')
+      .doc(id)
+      .update({
+        title: noteObject.title,
+        body: noteObject.body,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      });
+  };
+  newNote = async (title) => {
+    const note = { title: title, body: '' };
+    const newFromDb = await firebase
+      .firestore()
+      .collection('notes')
+      .add({
+        title: note.title,
+        body: note.body,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      });
+    const newId = newFromDb.id;
+    this.setState({ notes: [...this.state.notes, note] });
+    const newNoteIndex = this.state.notes.indexOf(
+      this.state.notes.filter((_note) => _note.id === newId)[0]
+    );
+    this.setState({
+      selectedNote: this.state.notes[newNoteIndex],
+      selectedNoteIndex: newNoteIndex
+    });
+  };
+  deleteNote = async (note) => {
+    const noteIndex = this.state.notes.indexOf(note);
+    await this.setState({
+      notes: this.state.notes.filter((_note) => _note !== note)
+    });
+    if (this.state.selectedNoteIndex === noteIndex) {
+      this.setState({ selectedNoteIndex: null, selectNote: null });
+    } else {
+      if (this.state.notes.length >= 1) {
+        this.state.selectedNoteIndex < noteIndex ?
+          this.selectNote(
+            this.state.notes[this.state.selectedNoteIndex ],
+            this.state.selectedNoteIndex  
+          ):
+        this.selectNote(
+          this.state.notes[this.state.selectedNoteIndex - 1],
+          this.state.selectedNoteIndex - 1
+        );
+      } else {
+        this.setState({ selectedNoteIndex: null, selectNote: null });
+      }
+    }
+    firebase
+      .firestore()
+      .collection('notes')
+      .doc(note.id)
+      .delete();
+  };
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+const firebase = require('firebase');
+require ('firebase/firestore');
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDa6sqccQUYjOEkwM5ZmQ4UHJ9o2UfXpyg",
+  authDomain: "evernote-clone-5e6f3.firebaseapp.com",
+  databaseURL: "https://evernote-clone-5e6f3.firebaseio.com",
+  projectId: "evernote-clone-5e6f3",
+  storageBucket: "evernote-clone-5e6f3.appspot.com",
+  messagingSenderId: "530874664627",
+  appId: "1:530874664627:web:fba1a69fd1af76796c52c6",
+  measurementId: "G-WRTSQSZYCE"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
